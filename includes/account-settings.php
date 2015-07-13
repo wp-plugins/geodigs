ae79b5f23299d55a9966c24df054cec10319e911
NewMediaOne <NewMediaOne@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 21:30:12 +0000

    tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/geodigs/tags/1.2@1198345 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/account-settings.php
+++ includes/account-settings.php
@@ -1,7 +1,8 @@
 <?php
 global $gd_api;
 
-$errors = array();
+$errors  = array();
+$success = false;
 
 if ($_POST) {
 	if ($_SESSION['gd_user']->email != $_POST['email'] && $_POST['emailConfirm'] != $_POST['email']) {
@@ -21,7 +22,7 @@ if ($_POST) {
 	
 	$_POST['phone'] = preg_replace('/\D+/', '', $_POST['phone']);
 	if (strlen($_POST['phone']) != 10) {
-			$errors['phone_length'] = 'Invalid phone number';
+		$errors['phone_length'] = 'Invalid phone number';
 	}
 	
 	if (count($errors) == 0) {
@@ -39,107 +40,16 @@ if ($_POST) {
 		$response = $gd_api->call('PATCH', 'users');
 		if (!isset($response->error)) {
 			$user_info = Geodigs_User::get_user_info($_SESSION['gd_user']->id);
-			Geodigs_User::update_user_info($user_info);
+			if (Geodigs_User::update_user_info($user_info)) {
+				$success = true;
+			}
 		}
 	}
 }
-?>
 
-<div class="wrap">
-	<form action="" id="geodigs-account-settings-form" class="gd-form-full" method="POST">
-		<?php if (count($errors) > 0): ?>
-			<div id="geodigs-account-settings-form-errors">
-			<?php foreach ($errors as $error): ?>
-				<span class="gd-error"><?=$error?></span><br>
-			<?php endforeach; ?>
-			</div>
-		<?php endif; ?>
-		<fieldset>
-			<h2>Details</h2>
-			<div class="pure-g">
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-first-name">First Name</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-first-name" name="firstName" value="<?=$_SESSION['gd_user']->name->first?>" required>
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-last-name">Last Name</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-last-name" name="lastName" value="<?=$_SESSION['gd_user']->name->last?>" required>
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-phone">Phone</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-phone" name="phone" value="<?=$_SESSION['gd_user']->phone->readable?>" required>
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-email">Email</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-email" name="email" value="<?=$_SESSION['gd_user']->email?>" required>
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-email-confirm"></label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-email-confirm" name="emailConfirm" placeholder="Confirm email">
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-password">Password</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="password" id="geodigs-account-settings-form-password" name="password">
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-password-confirm"></label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="password" id="geodigs-account-settings-form-password-confirm" name="passwordConfirm" placeholder="Confirm password">
-				</div>
-			</div>
-		</fieldset>
-		<fieldset>
-			<h2>Address</h2>
-			<div class="pure-g">
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-street">Street</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-street" name="addressStreet" value="<?=$_SESSION['gd_user']->address->street?>">
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-unit">Unit</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-unit" name="addressUnit" value="<?=$_SESSION['gd_user']->address->unit?>">
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-city">City</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-city" name="addressCity" value="<?=$_SESSION['gd_user']->address->city?>">
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-zip">Zip Code</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<input type="text" id="geodigs-account-settings-form-zip" name="addressZip" value="<?=$_SESSION['gd_user']->address->zip?>">
-				</div>
-				<div class="pure-u-md-1-4 pure-u-24-24">
-					<label for="geodigs-account-settings-form-state">State</label>
-				</div>
-				<div class="pure-u-md-3-4 pure-u-24-24">
-					<select id="geodigs-account-settings-form-state" name="addressState" value="<?=$_SESSION['gd_user']->address->state?>">
-						<?=gd_state_list($_SESSION['gd_user']->address->state)?>
-					</select>
-				</div>
-			</div>
-		</fieldset>
-		<div class="pure-controls">
-			<input type="submit" value="Update">
-        </div>
-	</form>
-</div>
+GeodigsTemplates::loadTemplate(
+	'account/settings.php',
+	array(
+		'errors' => $errors,
+		'success' => $success,
+	));
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: NewMediaOne
 Tags: real estate, idx, agents, colorado, listings
 Requires at least: 3.5
 Tested up to: 4.2.2
-Stable Tag: 1.1
+Stable tag: 1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -46,3 +46,8 @@ Contact us at http://www.newmediaone.net and we can get you started!
 = 1.1 =
 * Updates to Admin area
 * New templates
+
+= 1.2 =
+* Added alerts to Account Settings page
+* Added Account Settings template
+* Fixed "Favorite" button on listing details page
